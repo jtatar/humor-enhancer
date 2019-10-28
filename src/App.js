1ee6cda7acd503349b0abc28b70448a098085582
Metexo <13kubek@gmail.com>
2019-10-28 22:54:03 +0100

    Changing App to class

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import Register from './components/Register/Register'
 import Signin from './components/Signin/Signin'
 import './App.css';
 
-loadUser = () => {
 
-}
+class App extends Component {
+  constructor(){
+    super();
+  }
+  
+  loadUser = () => {
+  
+  }
+  
+  onRouteChange = () => {
+  
+  }
 
-onRouteChange = () => {
-
-}
-
-function App() {
-  return (
-    <div className="App">
-      <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-    </div>
-  );
+  render(){
+    return (
+      <div className="App">
+        <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+      </div>
+    );
+  }
 }
 
 export default App;
